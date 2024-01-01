f0cf61ba22551da49e5429717ae7eb131331b1b0
x1unix <denis0051@gmail.com>
2024-01-01 15:45:34 -0500

    fix: fix syntax highlight

--- .config/shell/include.d/00-root.sh
+++ .config/shell/include.d/00-root.sh
@@ -2,4 +2,4 @@ export EDITOR=nvim
 
 shell_path "$HOME/.local/bin"
 shell_source "$HOME/.cargo/env"
-shell_source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+[ -n "$HOMEBREW_PREFIX" ] && . "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
