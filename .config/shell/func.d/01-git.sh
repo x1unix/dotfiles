82e13f9d52334ccf1c0b863a46f3ec4aabdf779b
x1unix <9203548+x1unix@users.noreply.github.com>
2025-03-29 08:21:12 -0400

    feat(shell): add closebranch

--- .config/shell/func.d/01-git.sh
+++ .config/shell/func.d/01-git.sh
@@ -6,3 +6,9 @@ pubbranch() {
   git push --set-upstream origin $(cbranch)
 }
 
+closebranch() {
+  curbranch="$(git rev-parse --abbrev-ref HEAD)"
+  git checkout "$1" && git pull
+  git branch -D "$curbranch"
+}
+
