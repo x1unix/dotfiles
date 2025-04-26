86f694c38b7819ca000d77d0398e46640a815520
x1unix <9203548+x1unix@users.noreply.github.com>
2025-04-26 05:36:10 -0400

    feat: tmux

--- .config/shell/func.d/01-git.sh
+++ .config/shell/func.d/01-git.sh
@@ -12,3 +12,15 @@ closebranch() {
   git branch -D "$curbranch"
 }
 
+git-rename-branch() {
+  if [ $# -ne 2 ]; then
+    echo "Usage     : ${FUNCNAME[0]} <old branch name> <new branch name>"
+    echo "Example   : ${FUNCNAME[0]} master release"
+    return 1 
+  fi
+
+  git checkout "$1"
+  git branch -m "$2"
+  git push origin ":$1" "$2"
+  git push origin -u "$2"
+}
