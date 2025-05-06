95ef3fa7989e596b9763a6374adae5e6554b0608
x1unix <9203548+x1unix@users.noreply.github.com>
2025-05-05 20:07:56 -0400

    feat(emacs): font size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,17 @@
 ;; Based on https://github.com/rexim/dotfiles/blob/master/.emacs
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 (column-number-mode 1)
 (display-line-numbers-mode 1)
-(message "test?")
+(global-display-line-numbers-mode 1)
 
+;; Font
+(defun rc/get-default-font ()
+  (cond
+    ((eq system-type 'darwin) "ZedMono Nerd Font Mono-16")
+    ((eq system-type 'gnu/linux) "ZedMono Nerd Font Mono-16")
+  )
+)
+
+(add-to-list 'default-frame-alist `(font . ,(rc/get-default-font)))
 
