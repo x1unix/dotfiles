3da9ba2657bb5ce2f7ef8cec34281837e22648a7
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-29 18:01:55 -0400

    feat(nvim): neogit doesn't worth that...

--- common/config/nvim/lua/plugins/core/git.lua
+++ common/config/nvim/lua/plugins/core/git.lua
@@ -6,16 +6,4 @@ return {
       enabled = false,
     },
   },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim',
-
-      'nvim-telescope/telescope.nvim',
-      'ibhagwan/fzf-lua',
-      'echasnovski/mini.pick',
-      'folke/snacks.nvim',
-    },
-  },
 }
