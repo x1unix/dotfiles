b3c7d24bfe1fd06ce7ea7f9bb47106fe8fde108f
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-29 03:00:41 -0400

    fix(nvim): fix typo

--- common/config/nvim/lua/plugins/core/whichkey.lua
+++ common/config/nvim/lua/plugins/core/whichkey.lua
@@ -2,18 +2,18 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {},
     keys = {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "buffer local keymaps (which-key)",
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
     },
     config = function()
-      local wk = require("which-key")
-      wk.setup({})
+      require("which-key").setup({})
       require("keymap")
     end
-  },
+  }
 }
