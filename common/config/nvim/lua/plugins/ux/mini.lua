7a77bf85e9f92e6ee555f24b5868e8c151fc9998
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-29 17:49:16 -0400

    feat: add lazygit

--- common/config/nvim/lua/plugins/ux/mini.lua
+++ common/config/nvim/lua/plugins/ux/mini.lua
@@ -25,8 +25,14 @@ return {
       require('mini.files').setup({
         content = {
           filter = function(name)
-            local blocked =
-              { ['__pycache__'] = true, ['venv'] = true, ['.venv'] = true, ['.pytest_cache'] = true, ['.git'] = true, ['.idea'] = true }
+            local blocked = {
+              ['__pycache__'] = true,
+              ['venv'] = true,
+              ['.venv'] = true,
+              ['.pytest_cache'] = true,
+              ['.git'] = true,
+              ['.idea'] = true,
+            }
             return not blocked[name.name]
           end,
         },
