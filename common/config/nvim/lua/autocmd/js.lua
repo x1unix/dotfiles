1e8c8cdb45523354595b1ab972aef8470bd515b6
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-29 02:21:08 -0400

    feat(nvim): migrate lspsaga and autocmds

---
+++ common/config/nvim/lua/autocmd/js.lua
@@ -0,0 +1,41 @@
+-- Fix filetypes for tsx & jsx
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.tsx",
+  callback = function()
+    vim.bo.filetype = "typescriptreact"
+  end,
+})
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.jsx",
+  callback = function()
+    vim.bo.filetype = "javascriptreact"
+  end,
+})
+
+-- Call prettier
+-- local function is_prettier_file(fname)
+--   return fname:match('^.eslintrc')
+-- end
+--
+-- vim.api.nvim_create_autocmd('BufWritePost', {
+--   pattern = {
+--     '*.jsx', '*.tsx', '*.js', '*.ts',
+--   },
+--   callback = function(args)
+--     local bufnr = vim.api.nvim_get_current_buf()
+--     local eslintrc = vim.fs.find(is_prettier_file, {
+--       type = 'file',
+--       limit = 32,
+--       upward = true, -- Search upward through parent directories
+--       path = vim.fn.expand("%:p:h"), -- Start from the current file's directory
+--     })
+--
+--     if not eslintrc then
+--       vim.schedule(function ()
+--         vim.notify("ESLint config not found", vim.log.levels.WARN, { title = "ESLint" })
+--       end)
+--     end
+--     print(vim.inspect(eslintrc))
+--   end,
+-- })
--- common/config/nvim/lua/plugins/languages/init.lua
+++ common/config/nvim/lua/plugins/languages/init.lua
@@ -1,5 +1,7 @@
 local stubs = require("stubs")
 
 stubs.lazy_require("plugins.languages.lspconfig")
-stubs.lazy_require("plugins.languages.treesitter")
+stubs.lazy_require("plugins.languages.lspsaga")
 stubs.lazy_require("plugins.languages.prettier")
+stubs.lazy_require("plugins.languages.treesitter")
+stubs.lazy_require("plugins.languages.ts")
