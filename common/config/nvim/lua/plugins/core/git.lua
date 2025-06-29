2aeb0d7e049de86842203f8c928d471596e69a71
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-29 17:49:16 -0400

    feat: add lazygit

--- common/config/nvim/lua/plugins/core/git.lua
+++ common/config/nvim/lua/plugins/core/git.lua
@@ -6,4 +6,16 @@ return {
       enabled = false,
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+
+      'nvim-telescope/telescope.nvim',
+      'ibhagwan/fzf-lua',
+      'echasnovski/mini.pick',
+      'folke/snacks.nvim',
+    },
+  },
 }
--- common/config/nvim/lua/plugins/languages/mason.lua
+++ common/config/nvim/lua/plugins/languages/mason.lua
@@ -3,12 +3,18 @@ return {
     'mason-org/mason-lspconfig.nvim',
     opts = {
       ensure_installed = {
-        'lua_ls', 'rust_analyzer', 'gopls', 'terraformls', 'buf_ls',
-        'bashls', 'clangd', 'eslint',
-      }
+        'lua_ls',
+        'rust_analyzer',
+        'gopls',
+        'terraformls',
+        'buf_ls',
+        'bashls',
+        'clangd',
+        'eslint',
+      },
     },
     dependencies = {
-     {
+      {
         'mason-org/mason.nvim',
         opts = {},
       },
