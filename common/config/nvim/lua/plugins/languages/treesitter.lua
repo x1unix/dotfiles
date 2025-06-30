49da3a9b5b303bd3686e901bf22ec913d1d29723
x1unix <9203548+x1unix@users.noreply.github.com>
2025-06-30 10:00:10 -0400

    feat(nvim): add treesitter enhancements

--- common/config/nvim/lua/plugins/languages/treesitter.lua
+++ common/config/nvim/lua/plugins/languages/treesitter.lua
@@ -6,8 +6,53 @@ return {
     branch = 'master',
     lazy = false,
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'RRethy/nvim-treesitter-endwise',
+      'windwp/nvim-ts-autotag',
+    },
     opts = {
       ensure_installed = pkgs.languages,
+      textobjects = {
+        lsp_interop = {
+          enable = true,
+          border = 'none',
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ['<leader>df'] = '@function.outer',
+            ['<leader>dF'] = '@class.outer',
+          },
+        },
+        select = {
+          enable = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
+        matchup = {
+          enable = true,
+        },
+        endwise = {
+          enable = true,
+        },
+        autotag = {
+          enable = true,
+        },
+      },
     },
   },
 }
