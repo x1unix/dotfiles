7cf3916127bc473fe62037fda6a6397047d033d7
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-19 01:33:02 -0400

    feat(deploy): add termux target

---
+++ targets/termux.target.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+#require: os=linux distro=android
+#description: dotfiles and packages for Termux environment
+
+termux_pkg_install() {
+  pkg install -y \
+    x11-repo termux-tools termux-services unzip which build-essential \
+    inetutils htop iproute2 lsof net-tools nmap \
+    tmux proot-distro sed stow git openssh fzf ripgrep \
+    neovim zsh rust stylua nodejs golang lua-language-server
+}
+
+termux_init_bashrc() {
+  if ! grep -q '.bash_aliases' ~/.bashrc; then
+    echo 'ALIASES=$HOME/.bash_aliases' >> "$HOME/.bashrc"
+    echo 'if [ -f "$ALIASES" ]; then' >> "$HOME/.bashrc"
+    echo '  . "$ALIASES"' >> "$HOME/.bashrc"
+    echo 'fi' >> "$HOME/.bashrc"
+	else 
+		echo '.bash_aliases already installed, skip'
+	fi
+}
+
+termux_init_home() {
+  mkdir -p "$HOME/.local/bin"
+  termux-setup-storage
+}
+
+step termux_init_home
+step termux_init_bashrc
+step termux_pkg_install 'flag:pkgs'
+
+require common
+
+# Link .bashrc
+link_home dotfiles
+
+# Mount Termux config to ~/.termux
+link_home '.termux' termux
+
---
+++ termux/dotfiles/.bash_aliases
@@ -0,0 +1,22 @@
+set -o vi
+
+export PATH="$PATH:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/go/bin"
+
+alias ..='cd ..'
+alias ll='ls -la'
+alias apti='sudo apt install'
+alias apts='apt search'
+alias nv='nvim'
+alias tailf='tail -f'
+alias arch='proot-distro login --user x1unix archlinux'
+alias archr='proot-distro login archlinux'
+alias md='mkdir'
+
+mcd() {
+  mkdir "$1" && cd "$1"
+}
+
+# linuxbrew
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
