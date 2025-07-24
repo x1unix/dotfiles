77417e80dbc0ba72167085df4d1f4e567b2d4acb
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-23 23:49:25 -0400

    fix(dotfiles): use fusuma only for mbp

--- targets/arch.target.sh
+++ targets/arch.target.sh
@@ -22,8 +22,6 @@ arch_pacman_install() {
 		stow sway swaybg swayidle swaylock xorg-xwayland xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk wofi waybar wl-clipboard \
 		grim slurp kitty dolphin network-manager-applet networkmanager nm-connection-editor neovim git net-tools man-db \
 		gnome-keyring polkit-gnome blueman ttf-zed-mono-nerd zsh-syntax-highlighting zsh lazygit
-
-	paru ruby-fusuma
 }
 
 step arch_paru_install 'flag:pkgs'
--- targets/mbp-arch.target.sh
+++ targets/mbp-arch.target.sh
@@ -2,6 +2,10 @@
 #require: os=linux arch=x86_64 distro=arch
 #description: dotfiles and packages for Intel MacBook Pro on Arch Linux
 
+arch_pacman_extra() {
+	paru ruby-fusuma
+}
+
 link_xdg_config config
 require arch
-
+step arch_pacman_extra 'flag:pkgs'
