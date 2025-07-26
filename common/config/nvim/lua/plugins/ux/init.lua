d99d3b5214509b0d438a61277e0096afb351308b
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-26 16:13:50 -0400

    feat(nvim): use lsp-signature-help instead cmp

--- common/config/nvim/lua/plugins/ux/init.lua
+++ common/config/nvim/lua/plugins/ux/init.lua
@@ -1,6 +1,6 @@
 return {
   { import = 'plugins.ux.flash' },
-  -- { import = 'plugins.ux.lsp_signature' },
+  { import = 'plugins.ux.lsp_signature' },
   { import = 'plugins.ux.mini' },
   { import = 'plugins.ux.ufo' },
   { import = 'plugins.ux.trouble' },
