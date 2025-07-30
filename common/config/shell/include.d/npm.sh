413e775a2b7ec53443e2d7de11de749a375be07d
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-29 23:32:47 -0400

    fix(npm): prefix

--- common/config/shell/include.d/npm.sh
+++ common/config/shell/include.d/npm.sh
@@ -1,6 +1,6 @@
-NPM_PACKAGES="${HOME}/.npm-packages"
+NPM_PACKAGES="$HOME/.npm/bin"
 
-shell_path "$NPM_PACKAGES/bin"
+export PATH="$PATH:$NPM_PACKAGES"
 
 # Preserve MANPATH if you already defined it somewhere in your config.
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
