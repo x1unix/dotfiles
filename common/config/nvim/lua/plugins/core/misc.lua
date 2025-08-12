3fd1d5e9e1a5e3df530c1f4220b887c6c0703a3f
x1unix <9203548+x1unix@users.noreply.github.com>
2025-08-12 01:35:06 -0400

    feat(nvim): add indent-blankline.lua

--- common/config/nvim/lua/plugins/core/misc.lua
+++ common/config/nvim/lua/plugins/core/misc.lua
@@ -1,6 +1,26 @@
+local highlight = {
+  'CursorColumn',
+  'Whitespace',
+}
+
 return {
   {
     'numToStr/Comment.nvim',
     lazy = false,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      -- See ':highlight' command.
+      indent = {
+        highlight = highlight,
+      },
+      whitespace = {
+        highlight = highlight,
+      },
+    },
   },
 }
