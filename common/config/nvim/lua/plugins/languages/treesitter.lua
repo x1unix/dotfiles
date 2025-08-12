c9330520a32d4e181e3ff31da3877f8de8dbdfb3
x1unix <9203548+x1unix@users.noreply.github.com>
2025-08-12 02:11:15 -0400

    feat(nvim): add treesitter-context

--- common/config/nvim/lua/plugins/languages/treesitter.lua
+++ common/config/nvim/lua/plugins/languages/treesitter.lua
@@ -1,5 +1,3 @@
-local pkgs = require('config.lang_packages')
-
 return {
   {
     'nvim-treesitter/nvim-treesitter',
---
+++ common/config/nvim/lua/plugins/ux/ts_context.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    version = '^1.0.0',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      enable = true,
+      multiwindow = true,
+    },
+  },
+}
