994af69cb1c61dd3871bbf61a6760fd910fd27d0
x1unix <9203548+x1unix@users.noreply.github.com>
2025-08-24 17:26:53 -0400

    feat(nvim): add snippets

--- common/config/nvim/lua/plugins/core/luasnip.lua
+++ common/config/nvim/lua/plugins/core/luasnip.lua
@@ -3,5 +3,14 @@ return {
     'L3MON4D3/LuaSnip',
     version = '^2.4.0',
     build = 'make install_jsregexp',
+    event = 'InsertEnter *',
+    dependencies = {
+      'stevearc/vim-vscode-snippets',
+      'rafamadriz/friendly-snippets',
+    },
+    config = function()
+      require('luasnip').setup()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
   },
 }
