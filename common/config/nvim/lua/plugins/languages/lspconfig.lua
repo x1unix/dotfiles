2b7a45f62c44836dfa1acea54373ff37280aea31
x1unix <9203548+x1unix@users.noreply.github.com>
2025-09-19 13:05:50 -0400

    feat(nvim): support lspconfig for nvim 0.11+

--- common/config/nvim/lua/plugins/languages/lspconfig.lua
+++ common/config/nvim/lua/plugins/languages/lspconfig.lua
@@ -4,22 +4,13 @@
 --   * https://github.com/radleylewis/nvim/blob/7e0001ae2c4c206f7b42da2866f8a6deaf0423c9/lua/plugins/nvim-lspconfig.lua#L64
 --   * https://github.com/YasTheGoat/NeoConf/blob/de3cc569fef8e8854ba585b29a1f0ad58b9df1ad/lua/plugins/lsp-config.lua#L37
 --
-local auto_enable = {
-  'html',
-  'emmet_language_server',
-  'cssls',
-  'tailwindcss',
-  'yamlls',
-  'jsonls',
-}
-
 return {
   {
     'neovim/nvim-lspconfig',
     dependencies = { 'saghen/blink.cmp' },
     config = function()
-      local lspconfig = require('lspconfig')
-      local capabilities = require('util.lsp').make_capabilities()
+      local lsputil = require('util.lsp')
+      local capabilities = lsputil.make_capabilities()
 
       -- Modify capabilities with folding range for nvim-ufo.
       -- capabilities.textDocument.foldingRange = {
@@ -35,27 +26,61 @@ return {
       --   capabilities = capabilities,
       -- })
 
-      lspconfig.terraformls.setup({
-        capabilities = capabilities,
-      })
+      lsputil.config({
+        terraformls = {
+          capabilities = capabilities,
+        },
+        html = { capabilities = capabilities },
+        emmet_language_server = { capabilities = capabilities },
+        cssls = { capabilities = capabilities },
+        tailwindcss = { capabilities = capabilities },
+        yamlls = { capabilities = capabilities },
+        jsonls = { capabilities = capabilities },
+        clangd = {
+          -- Fix filetypes for clangd. See: https://github.com/LazyVim/LazyVim/discussions/3997
+          filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'hpp' },
+        },
+        eslint = {
+          on_attach = function(_client, bufnr)
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = bufnr,
+              command = 'EslintFixAll',
+            })
+          end,
+        },
+        buf_ls = {
+          -- Sometimes Mason's buf_ls doesn't start
+          capabilities = capabilities,
+        },
+        lua_ls = {
+          capabilities = capabilities,
+          settings = {
+            Lua = {},
+          },
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                return
+              end
+            end
 
-      -- Fix filetypes for clangd. See: https://github.com/LazyVim/LazyVim/discussions/3997
-      lspconfig.clangd.setup({
-        filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'hpp' },
-      })
-
-      lspconfig.eslint.setup({
-        on_attach = function(_client, bufnr)
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = bufnr,
-            command = 'EslintFixAll',
-          })
-        end,
-      })
-
-      -- Sometimes Mason's buf_ls doesn't start
-      lspconfig.buf_ls.setup({
-        capabilities = capabilities,
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                version = 'LuaJIT',
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  -- Depending on the usage, you might want to add additional paths here.
+                  vim.env.VIMRUNTIME,
+                  -- "${3rd}/luv/library"
+                  -- "${3rd}/busted/library",
+                },
+              },
+            })
+          end,
+        },
       })
 
       -- Run ":checkhealth lsp" to see the status or to troubleshoot.
@@ -69,43 +94,6 @@ return {
       --     ['rust-analyzer'] = {},
       --   },
       -- })
-
-      -- Enable another servers manually as mason-lspconfig autoenable is disabled.
-      for _, server in ipairs(auto_enable) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-        })
-      end
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {},
-        },
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-              return
-            end
-          end
-
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              version = 'LuaJIT',
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                -- Depending on the usage, you might want to add additional paths here.
-                vim.env.VIMRUNTIME,
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
-              },
-            },
-          })
-        end,
-      })
     end,
   },
 }
--- common/config/nvim/lua/util/lsp.lua
+++ common/config/nvim/lua/util/lsp.lua
@@ -5,4 +5,36 @@ M.make_capabilities = function()
   return require('blink.cmp').get_lsp_capabilities({}, true)
 end
 
+-- Returns lspconfig object.
+--
+-- Uses 'vim.lsp.config' since nvim 0.11+ or falls back to require('lspconfig').
+M.lspconfig = function()
+  if vim.lsp and vim.lsp.config then
+    -- nvim 0.11+
+    return vim.lsp.config
+  end
+
+  return require('lspconfig')
+end
+
+--- Setups specified language servers.
+---
+--- Function is compatibility wrapper around vim.lsp.config introduced in v0.11+ and legacy 'lspconfig' module.
+--- @param kv table<string, vim.lsp.Config>
+M.config = function(kv)
+  -- nvim 0.11+
+  if vim.lsp and vim.lsp.config then
+    for name, cfg in pairs(kv) do
+      vim.lsp.config(name, cfg)
+      vim.lsp.enable(name)
+    end
+    return
+  end
+
+  local lspconfig = require('lspconfig')
+  for name, cfg in pairs(kv) do
+    lspconfig[name].setup(cfg)
+  end
+end
+
 return M
