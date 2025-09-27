d1c7ac0935c1b592f0981fe003c9cb97de9ce287
x1unix <9203548+x1unix@users.noreply.github.com>
2025-09-27 18:42:17 -0400

    feat(arch): add wallpapers

--- targets/arch.target.sh
+++ targets/arch.target.sh
@@ -3,26 +3,26 @@
 #description: dotfiles and packages for Arch Linux
 
 arch_paru_install() {
-	if command -v paru >/dev/null 2>&1; then
-		echo "Paru already installed, skip"
-		return
-	fi
+  if command -v paru >/dev/null 2>&1; then
+    echo "Paru already installed, skip"
+    return
+  fi
 
-	sudo pacman -S base-devel git rustup --noconfirm --needed
-	rustup default stable
-	git clone https://aur.archlinux.org/paru.git /tmp/paru-src
-	cd /tmp/paru-src
-	makepkg -si
-	cd -
-	rm -rf /tmp/paru-src
+  sudo pacman -S base-devel git rustup --noconfirm --needed
+  rustup default stable
+  git clone https://aur.archlinux.org/paru.git /tmp/paru-src
+  cd /tmp/paru-src
+  makepkg -si
+  cd -
+  rm -rf /tmp/paru-src
 }
 
 arch_pacman_install() {
-	sudo pacman -S \
-		sway swaybg swayidle swaylock dunst xorg-xwayland \
+  sudo pacman -S \
+    sway swaybg swayidle swaylock dunst xorg-xwayland \
     xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk gnome-keyring polkit-gnome blueman ttf-zed-mono-nerd \
-    hyprland hyprcursor hypridle hyprutils hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset \
-		stow wofi waybar wl-clipboard alacritty grim slurp kitty dolphin network-manager-applet networkmanager nm-connection-editor \
+    hyprland hyprcursor hypridle hyprutils hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset hyprpaper \
+    stow wofi waybar wl-clipboard alacritty grim slurp kitty dolphin network-manager-applet networkmanager nm-connection-editor \
     qt5-wayland qt6-wayland \
     tmux zsh-syntax-highlighting zsh net-tools man-db lf \
     git cmake nodejs npm go fzf ripgrep lazygit neovim \
@@ -31,8 +31,12 @@ arch_pacman_install() {
   gsettings set org.blueman.general notification-daemon true
 }
 
+# Deps
 step arch_paru_install 'flag:pkgs'
 step arch_pacman_install 'flag:pkgs'
-require common
-link_xdg_config config
 
+# Wallpapers
+link_home 'Pictures/wallpapers' 'wallpapers'
+
+# Configs
+link_xdg_config config
