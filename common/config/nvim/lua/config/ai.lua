7b8410753e1af8c74d0914e79ad28d4aec511821
x1unix <9203548+x1unix@users.noreply.github.com>
2025-10-06 21:56:41 -0400

    feat(nvim): add openrouter configs

--- common/config/nvim/lua/config/ai.lua
+++ common/config/nvim/lua/config/ai.lua
@@ -15,5 +15,13 @@ return {
         },
       },
     },
+    providers = {
+      openrouter = {
+        __inherited_from = 'openai',
+        endpoint = 'https://openrouter.ai/api/v1',
+        api_key_name = 'OPENROUTER_API_KEY',
+        model = 'anthropic/claude-sonnet-4',
+      },
+    },
   },
 }
