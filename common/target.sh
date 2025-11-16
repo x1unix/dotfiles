f8aa2c3fa814a87742e316240ffe011024d3caf2
x1unix <9203548+x1unix@users.noreply.github.com>
2025-11-15 22:28:30 -0500

    fix(common): indent

--- common/target.sh
+++ common/target.sh
@@ -2,29 +2,29 @@
 #description: common dotfiles
 
 install_shmgr() {
-	if ! grep -q '~/.config/shell/loader.sh' ~/.zshrc; then
-		echo '[ -f ~/.config/shell/loader.sh ] && . ~/.config/shell/loader.sh' >> ~/.zshrc
-	else 
-		echo 'shmgr autoload already installed, skip'
-	fi
+  if ! grep -q '~/.config/shell/loader.sh' ~/.zshrc; then
+    echo '[ -f ~/.config/shell/loader.sh ] && . ~/.config/shell/loader.sh' >>~/.zshrc
+  else
+    echo 'shmgr autoload already installed, skip'
+  fi
 
-	"$HOME/.local/bin/shmgr" gen
+  "$HOME/.local/bin/shmgr" gen
 }
 
 install_tpm() {
-	if [ ! -d ~/.tmux/plugins/tpm ]; then
-		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	else
-		echo "tpm already installed, skip"
-	fi
+  if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  else
+    echo "tpm already installed, skip"
+  fi
 
-	echo 'installing tpm plugins...'
+  echo 'installing tpm plugins...'
   export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins"
   $HOME/.tmux/plugins/tpm/bin/install_plugins
 }
 
 install_tpm_rollback() {
-	rm -rf "$HOME/.tmux/plugins/tpm"
+  rm -rf "$HOME/.tmux/plugins/tpm"
 }
 
 # Mount all dotfiles in common/dotfiles
@@ -46,4 +46,3 @@ step install_tpm 'flag:tpm'
 SSH_CONFIGS_DIR="$HOME/.ssh/config.d"
 sops_decrypt 'ssh_config.enc' "$SSH_CONFIGS_DIR/hosts"
 ssh_config_include "$SSH_CONFIGS_DIR/hosts"
-
