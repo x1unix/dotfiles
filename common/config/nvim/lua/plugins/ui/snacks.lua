b64817284272caf39dd92225eaeda4a4488a591f
x1unix <9203548+x1unix@users.noreply.github.com>
2025-12-11 13:39:59 -0500

    feat(nvim): add trouble to snacks

--- common/config/nvim/lua/plugins/ui/snacks.lua
+++ common/config/nvim/lua/plugins/ui/snacks.lua
@@ -3,15 +3,33 @@ return {
     'folke/snacks.nvim',
     priority = 1000,
     lazy = false,
-    opts = {
-      lazygit = {},
-      input = {},
-      image = {},
-      styles = {
-        input = {
-          border = 'rounded',
-        },
-      },
+    dependencies = {
+      'folke/trouble.nvim',
     },
+    opts = function(_, opts)
+      return vim.tbl_deep_extend('force', opts or {}, {
+        lazygit = {},
+        input = {},
+        image = {},
+        styles = {
+          input = {
+            border = 'rounded',
+          },
+        },
+        picker = {
+          actions = require('trouble.sources.snacks').actions,
+          win = {
+            input = {
+              keys = {
+                ['<c-t>'] = {
+                  'trouble_open',
+                  mode = { 'n', 'i' },
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
   },
 }
