6cba116471e6d095997c825b23cb9a6f2414fbd4
x1unix <9203548+x1unix@users.noreply.github.com>
2025-12-17 19:08:53 -0500

    fix(nvim): mini.animate is a buggy mess

--- common/config/nvim/lua/plugins/ux/mini.lua
+++ common/config/nvim/lua/plugins/ux/mini.lua
@@ -110,28 +110,6 @@ local mini_plugins = {
       width_preview = 50,
     },
   },
-
-  -- Animations (only for jumps).
-  ['mini.animate'] = function(animate)
-    return {
-      cursor = {
-        enable = false,
-      },
-      scroll = {
-        enable = true,
-        timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-      },
-      resize = {
-        enable = false,
-      },
-      open = {
-        enable = false,
-      },
-      close = {
-        enable = false,
-      },
-    }
-  end,
 }
 
 return {
