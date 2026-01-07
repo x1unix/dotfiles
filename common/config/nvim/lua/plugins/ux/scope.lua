9d12affca1c18a778c831fd83304e33851b84119
x1unix <9203548+x1unix@users.noreply.github.com>
2026-01-07 18:04:33 -0500

    fix(nvim): attempt to fix scope.nvim

--- common/config/nvim/lua/plugins/ux/scope.lua
+++ common/config/nvim/lua/plugins/ux/scope.lua
@@ -3,6 +3,7 @@ return {
     -- Scoped buffers.
     -- Used to separate buffers between separate windows and keep bufferline clear.
     'tiagovla/scope.nvim',
-    config = true,
+    lazy = false,
+    opts = {},
   },
 }
