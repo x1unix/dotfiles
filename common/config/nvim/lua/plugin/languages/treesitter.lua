897ff9fddb9f8cbba2447d90f1825fd6319a9175
x1unix <9203548+x1unix@users.noreply.github.com>
2026-04-01 19:43:31 -0400

    fix(nvim): manually install missing parsers
    Signed-off-by: x1unix <9203548+x1unix@users.noreply.github.com>

--- common/config/nvim/lua/plugin/languages/treesitter.lua
+++ common/config/nvim/lua/plugin/languages/treesitter.lua
@@ -1,5 +1,5 @@
----@module 'nvim-treesitter.parsers'
----@param parsers table<string, ParserInfo> | nil
+---@module 'nvim-ts.parsers'
+---@param parsers nvim-ts.parsers | nil
 local function register_parsers(parsers)
   if not parsers then
     return
@@ -11,6 +11,42 @@ local function register_parsers(parsers)
   end
 end
 
+local function ts_get_missing()
+  local ts = require('nvim-treesitter')
+  local cfg = require('config.languages.grammars')
+
+  local installed = ts.get_installed()
+  if #installed == 0 then
+    return cfg.grammars
+  end
+
+  local install_set = {}
+  for _, v in ipairs(installed) do
+    install_set[v] = true
+  end
+
+  local to_install = {}
+  for _, e in ipairs(cfg.grammars) do
+    if not install_set[e] then
+      to_install[#to_install + 1] = e
+    end
+  end
+
+  return to_install
+end
+
+local function ts_sync()
+  local ts = require('nvim-treesitter')
+
+  local to_install = ts_get_missing()
+  if #to_install == 0 then
+    return
+  end
+
+  vim.notify('Installing ' .. tostring(#to_install) .. ' grammars...', vim.log.levels.INFO)
+  ts.install(to_install)
+end
+
 return {
   {
     'nvim-treesitter/nvim-treesitter',
@@ -18,18 +54,26 @@ return {
     -- TODO: remove commit when nvim will be upgraded to v0.12
     commit = '90cd6580e720caedacb91fdd587b747a6e77d61f',
     lazy = false,
-    build = ':TSUpdate',
+    build = ':TSUpdate | TSSync',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
       'RRethy/nvim-treesitter-endwise',
       'windwp/nvim-ts-autotag',
     },
     opts = function()
-      local cfg = require('config.languages.grammars')
-      register_parsers(cfg.parsers)
+      -- Add helper to install missing grammars
+      vim.api.nvim_create_user_command('TSSync', ts_sync, { nargs = 0, desc = 'Install missing TreeSitter grammars' })
+      -- Load extra parsers after load
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'TSUpdate',
+        callback = function()
+          local cfg = require('config.languages.grammars')
+          register_parsers(cfg.parsers)
+        end,
+      })
 
       return {
-        ensure_installed = cfg.grammars,
+        install_dir = vim.fn.stdpath('data') .. '/site',
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false,
@@ -78,6 +122,12 @@ return {
     end,
     config = function(_, opts)
       require('nvim-treesitter').setup(opts)
+
+      -- Since nvim v0.11 and treesitter 'main' branch, ensure_installed doesn't work.
+      -- Manually ensure that grammars are installed.
+      --
+      -- P.S - What a shame. This is why we can't have nice things.
+      ts_sync()
     end,
   },
 }
