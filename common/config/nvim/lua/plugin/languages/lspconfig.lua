e153ad8d3411b29f69f223a13a25c84bb4462c27
x1unix <9203548+x1unix@users.noreply.github.com>
2026-05-12 17:53:27 -0400

    fix(nvim): call init for lspconfig

--- common/config/nvim/lua/plugin/languages/lspconfig.lua
+++ common/config/nvim/lua/plugin/languages/lspconfig.lua
@@ -1,8 +1,15 @@
 return {
   {
     'neovim/nvim-lspconfig',
+    lazy = false,
     dependencies = { 'saghen/blink.cmp' },
     config = function()
+      -- The plugin is packaged as a native neovim PM plugin,
+      -- placing bootstrap code into '/plugin' instead of classic '/lua/lspconfig/init.lua'.
+      --
+      -- lazy.vim doesn't call '/plugin' dir runtime files.
+      -- Call it manually to init the plugin.
+      vim.cmd('runtime plugin/lspconfig.lua')
       require('config.lsp').setup()
     end,
   },
