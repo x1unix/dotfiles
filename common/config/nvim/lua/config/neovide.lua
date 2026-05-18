8ba8a7a31cc50d40770be3091f69fed5cc140674
x1unix <9203548+x1unix@users.noreply.github.com>
2026-05-17 22:11:45 -0400

    fix(neovide): set font for macs

--- common/config/nvim/lua/config/neovide.lua
+++ common/config/nvim/lua/config/neovide.lua
@@ -1,8 +1,13 @@
+local osutil = require('util.os')
+
+-- Macs are usually laptops and having a bigger font is appreciated.
+local font_size = osutil.is_darwin() and '14' or '11'
+
 vim.g.neovide_font_hinting = 'none'
 vim.g.neovide_font_edging = 'subpixelantialias'
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_scroll_animation_length = 0.1
-vim.o.guifont = 'IosevkaTerm Nerd Font:h11:w-0.5:#e-subpixelantialias:#h-none'
+vim.o.guifont = 'IosevkaTerm Nerd Font:h' .. font_size .. ':w-0.5:#e-subpixelantialias:#h-none'
 
 -- HACK: when nvim started via desktop entry or inside Neovide, PATH isn't initialized.
 require('util.env').append_paths('~/.local/bin', '~/go/bin')
